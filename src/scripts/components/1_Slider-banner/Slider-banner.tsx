7b0ee592fe0485f786b7f8a998fb9a204df37d52
Vitaly Pasechnik <Vitaly.Pasechnik.11@gmail.com>
2022-10-08 13:28:23 +0300

    LDS

--- src/scripts/components/1_Slider-banner/Slider-banner.tsx
+++ src/scripts/components/1_Slider-banner/Slider-banner.tsx
@@ -41,7 +41,7 @@ export const SliderBanner: React.FC<Props> = ({ image }) => {
         <div className="slider-banner__main">
           <img
             className="slider__main-photos"
-            src={`../../img/Slider/${image[num]}`}
+            src={`img/Slider/${image[num]}`}
             alt={image[num]}
           />
         </div>
